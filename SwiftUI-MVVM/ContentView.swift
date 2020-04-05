a085d8e1438ec06230f1466bf9b1b2176ce51498
Bisera Belkoska <Bisera.Belkoska@netcetera.com>
2020-04-05 11:14:34 +0200

    Initial Commit

---
+++ SwiftUI-MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-MVVM
+//
+//  Created by Bisera Belkoska on 4/5/20.
+//  Copyright © 2020 Netcetera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
